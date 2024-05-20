6fd479aa3a2f021a9fb5e977516cd12f5b0ea71b
Piotyrius <mawanwala91@gmail.com>
2024-05-20 15:01:46 +0400

    Create Data.py

---
+++ Scrapper/Data.py
@@ -0,0 +1,3 @@
+URL = "https://www.shazam.com/song/1440823879/Flowers?referrer=browserextension"
+song_name = ""
+path = ""
---
+++ Scrapper/runner.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+driver = webdriver.Chrome('./chromedriver')
+
+driver.get("https://www.python.org")
